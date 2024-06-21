07ffe841996325a8a73b1e04f34f734ebc787e22
douglaskaplan <douglasjkaplan@gmail.com>
2024-06-20 21:07:55 -0500

    working on various queries

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -17,11 +17,23 @@
 
 -- FROM app_store_apps
 
-select distinct(a.name) as app_name, a.primary_genre as genre, a.rating as a_rating, p.rating as p_rating, round(p.rating + a.rating) as avg_rating, p.price
-from app_store_apps as a
-inner join play_store_apps as p
-using (name)
-where a.price = 0
-and p.price = '0'
-and (p.rating + a.rating) >= 5
-order by avg_rating desc;
+-- select distinct(a.name) as app_name, a.primary_genre as genre, a.rating as a_rating, p.rating as p_rating, round(p.rating + a.rating) as avg_rating, p.price
+-- from app_store_apps as a
+-- inner join play_store_apps as p
+-- using (name)
+-- where a.price = 0
+-- and p.price = '0'
+-- and (p.rating + a.rating) >= 10
+-- order by avg_rating desc;
+
+with astore as 
+(select name, price as price_in_money, rating from app_store_apps),
+pstore as (select name, cast(replace(price, '$', '') as decimal(10,2)) as price_in_money, rating from play_store_apps,
+bothapps as 
+select (coalesce(a.name, p.name) as appname,
+greatest(coalesce(a.price, 0), coalesce(p.price, 0)) as highprice
+coalesce(a.rating, 0) AS app_store_rating,
+coalesce(p.rating, 0) as play_store_rating.
+	from app_store_apps as a 
+full outer join play_store_apps as p
+using(name))
