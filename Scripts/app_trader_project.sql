3e58b810b5524d94141c17488e3b852ee0b63dc4
Zavello <zechariahavello@gmail.com>
2024-06-20 22:12:16 -0400

    apptrader_v2

--- Scripts/app_trader_project.sql
+++ Scripts/app_trader_project.sql
@@ -103,9 +103,77 @@
 -- Where app_store_apps.primary_genre ='Medical'
 -- Order By app_store_apps.price, app_store_apps.rating DESC;
 
-Select content_rating,
-	Count(name) As num_of_apps
+-- Select content_rating,
+-- 	Count(name) As num_of_apps
+-- From app_store_apps
+-- Where primary_genre = 'Games'
+-- Group By content_rating
+-- Order By num_of_apps DESC;
+
+-- 2079 apps, on Apple Store, that are games rated 4+ (or for everyone)
+
+-- Select content_rating,
+-- 	Count(name) As num_of_apps
+-- From play_store_apps
+-- Where category = 'GAME' Or category = 'FAMILY'
+-- Group By content_rating
+-- Order By num_of_apps DESC;
+
+-- 2137 apps, specifically on play store, that are games rated fro everyone
+
+Select Distinct app_store_apps.name As app_name,
+	app_store_apps.primary_genre As genre,
+	app_store_apps.rating As app_store_rating,
+	play_store_apps.rating As play_store_rating,
+	Round(Round((play_store_apps.rating + app_store_apps.rating),0)/2,1) As avg_rating
 From app_store_apps
-Where primary_genre = 'Games'
-Group By content_rating
-Order By num_of_apps DESC;
+Inner Join play_store_apps
+On app_store_apps.name = play_store_apps.name
+Where app_store_apps.price = 0
+	And play_store_apps.price = '0'
+	And Round(Round((play_store_apps.rating + app_store_apps.rating),0)/2,1) = 5	
+Order By avg_rating DESC;
+
+-- Top 6 apps that are free and rated an average of 5.0 stars.
+
+Select Distinct app_store_apps.name As name,
+	Cast(app_store_apps.review_count As INT) As apple_reviews,
+	Cast(Left(Replace(play_store_apps.install_count,',',''), Length(Replace(play_store_apps.install_count,',','')) - 1) As INT) as google_installs
+From app_store_apps
+Inner Join play_store_apps
+On app_store_apps.name = play_store_apps.name
+Where app_store_apps.price = 0
+	And play_store_apps.price = '0'
+Order By apple_reviews DESC, google_installs DESC;
+
+-- Select Left(Replace(play_store_apps.install_count,',',''), Length(Replace(play_store_apps.install_count,',','')) - 1) As google_installs
+-- From play_store_apps;
+
+Select Distinct app_store_apps.name As app_name,
+	app_store_apps.primary_genre As genre,
+	app_store_apps.rating As app_store_rating,
+	play_store_apps.rating As play_store_rating,
+	Round(Round((play_store_apps.rating + app_store_apps.rating),0)/2,1) As avg_rating,
+	Cast(app_store_apps.review_count As INT) As apple_reviews,
+	Cast(Left(Replace(play_store_apps.install_count,',',''), Length(Replace(play_store_apps.install_count,',','')) - 1) As INT) as google_installs,
+	app_store_apps.price As apple_price,
+	play_store_apps.price As google_price
+From app_store_apps
+Inner Join play_store_apps
+On app_store_apps.name = play_store_apps.name
+Where Round(Round((play_store_apps.rating + app_store_apps.rating),0)/2,1) >= 4.5
+Order By avg_rating DESC, google_installs DESC, apple_reviews DESC
+Limit 11;
+
+-- Select Distinct app_store_apps.name As app_name,
+-- 	app_store_apps.primary_genre As genre,
+-- 	app_store_apps.rating As app_store_rating,
+-- 	play_store_apps.rating As play_store_rating,
+-- 	Round(Round((play_store_apps.rating + app_store_apps.rating),0)/2,1) As avg_rating,
+-- 	app_store_apps.price As apple_price,
+-- 	play_store_apps.price As google_price
+-- From app_store_apps
+-- Inner Join play_store_apps
+-- On app_store_apps.name = play_store_apps.name
+-- Where Round(Round((play_store_apps.rating + app_store_apps.rating),0)/2,1) = 5	
+-- Order By avg_rating DESC;
