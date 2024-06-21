461dd2aa8e7c2c20b97be1ad598bcef7c51d65a5
douglaskaplan <douglasjkaplan@gmail.com>
2024-06-20 20:47:44 -0500

    got query down to 6

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -1,21 +1,27 @@
+-- select distinct app_store_apps.name as app_name,
+-- app_store_apps.primary_genre as genre,
+-- app_store_apps.rating as app_store_rating,
+-- play_store_apps.rating as play_store_rating,
+-- round(round((play_store_apps.rating + app_store_ratings.rating),0)/2,1) as avg_rating
+-- 	from app_store_apps
+-- 	inner join play_store_apps
+-- 	on app_store_apps.name = play_store_apps.name
+-- 	where app_store_apps.price = 0
+-- 	and play_store_apps.price = '0'
+-- 	and round(round((play_store_apps.rating + app_store_apps.rating),0)/2,1) = 5
+-- 	order by avg_rating desc;
 
--- with app_store as 
--- (select name, cast(replace(price, '$', '') as decimal (10,2)) as new_price 
--- from app_store_apps),
--- play_store as 
--- (select name, cast(replace(price, '$', '') as decimal (10,2)) as new_price 
--- from play_store_apps),
--- combined_apps as
--- (select (app_store,play_store) as app_name,
--- 	greatest(a.new_price) , (p.new_price) as high_price
--- from app_store_apps as a
--- full outer join
--- play_store_apps as p on a.name = p.name)
--- select name, high_price,
--- case when high_price <=1 then 10000
--- else high_price * 10000
--- end as purchase_price
--- from combined_apps
+-- SELECT DISTINCT app_store_apps.name AS app_name,
+	
+-- 	select round(play_store_apps.rating + app_store_apps.rating),0,1) AS avg_rating
 
-select distinct(genres), price from play_store_apps
-where genres like '%;Education%'
+-- FROM app_store_apps
+
+select distinct(a.name) as app_name, a.primary_genre as genre, a.rating as a_rating, p.rating as p_rating, round(p.rating + a.rating) as avg_rating, p.price
+from app_store_apps as a
+inner join play_store_apps as p
+using (name)
+where a.price = 0
+and p.price = '0'
+and (p.rating + a.rating) >= 5
+order by avg_rating desc;
