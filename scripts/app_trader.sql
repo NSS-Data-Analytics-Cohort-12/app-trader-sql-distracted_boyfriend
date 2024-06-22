5cc88829c1a7951fe31d18b692e9a72e575ecb6e
douglaskaplan <douglasjkaplan@gmail.com>
2024-06-22 10:55:34 -0500

    final query

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -96,7 +96,43 @@
 -- Order By avg_rating DESC, apple_review DESC, google_install DESC
 -- limit 10;
 
-Select distinct(google_name),
+-- Select distinct(google_name),
+-- 	google_price,
+-- 	google_rating, apple_name,
+-- 	apple_price, apple_rating,
+-- 	google_cost, apple_cost,
+-- 	apple_review,
+-- 	google_install,
+-- 	avg_rating,
+-- 	marketing_cost,
+-- 	(Case When apple_cost > google_cost
+-- 	Then apple_cost
+-- 	When apple_cost < google_cost
+-- 	Then google_cost
+-- 	When apple_cost = google_cost
+-- 	Then apple_cost End)As highest_cost
+-- From(Select google.name As google_name,google.price As google_price,google.rating As google_rating,apple.name As apple_name,apple.price As apple_price,apple.rating As apple_rating,
+-- 	Cast(apple.review_count As INT) As apple_review,
+-- 	Cast(Left(Replace(google.install_count,',',''), Length(Replace(google.install_count,',','')) - 1) As INT) as google_install,
+-- 	Round(Round((google.rating + apple.rating),0)/2,1) As avg_rating,
+-- 	(((Round(Round((google.rating + apple.rating),0)/2,1))*24)+12)*1000 As marketing_cost,
+-- 	Case When apple.price <= 1
+-- 	Then 10000
+-- 	When (apple.price  > 1)
+-- 	Then (apple.price * 10000) End As apple_cost,
+-- 	Case When Cast(replace(google.price,'$','') As numeric) <= 1
+-- 	Then 10000
+-- 	When Cast(replace(google.price,'$','') As numeric) > 1
+-- 	Then Cast(replace(google.price,'$','') As numeric)* 10000 End As google_cost
+-- From play_store_apps As google
+-- Inner Join app_store_apps AS apple
+-- On google.name = apple.name
+-- -- Where apple.price < '1.00'
+-- -- 	and google.price < '1.00'
+-- order by apple.rating desc,google.rating desc)
+-- Order By avg_rating DESC, apple_review DESC, google_install DESC;
+
+Select Distinct(google_name),
 	google_price,
 	google_rating, apple_name,
 	apple_price, apple_rating,
@@ -105,6 +141,7 @@ Select distinct(google_name),
 	google_install,
 	avg_rating,
 	marketing_cost,
+	revenue,
 	(Case When apple_cost > google_cost
 	Then apple_cost
 	When apple_cost < google_cost
@@ -116,6 +153,7 @@ From(Select google.name As google_name,google.price As google_price,google.ratin
 	Cast(Left(Replace(google.install_count,',',''), Length(Replace(google.install_count,',','')) - 1) As INT) as google_install,
 	Round(Round((google.rating + apple.rating),0)/2,1) As avg_rating,
 	(((Round(Round((google.rating + apple.rating),0)/2,1))*24)+12)*1000 As marketing_cost,
+	(((Round(Round((google.rating + apple.rating),0)/2,1))*24)+12)*10000 As revenue,
 	Case When apple.price <= 1
 	Then 10000
 	When (apple.price  > 1)
@@ -130,4 +168,5 @@ On google.name = apple.name
 -- Where apple.price < '1.00'
 -- 	and google.price < '1.00'
 order by apple.rating desc,google.rating desc)
-Order By avg_rating DESC, apple_review DESC, google_install DESC;
+Order By avg_rating DESC, apple_review DESC, google_install DESC
+limit 10;
